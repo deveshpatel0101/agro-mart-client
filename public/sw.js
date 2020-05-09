b95ed6f204a76723ce0c6d0b211cfa017337befc
computer0101 <deveshpatel7643@gmail.com>
2020-05-09 22:37:46 +0530

    update packages

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,5 @@
-self.addEventListener('install', function(event) {
-});
+self.addEventListener('install', function (event) {});
 
-self.addEventListener('activate', function(event) {
-});
+self.addEventListener('activate', function (event) {});
 
-self.addEventListener('fetch', function(event) {
-});
+self.addEventListener('fetch', function (event) {});
